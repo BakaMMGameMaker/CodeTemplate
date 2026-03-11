057c45344518829cfd2d22f95f0f00c7dc4f67ee
Fuxuanmylove <2793275118@qq.com>
2026-03-12 00:21:31 +0800

    长度至少为K的子数组的最大和

---
+++ Presum/SubsequenceWithBiggestSum.cpp
@@ -0,0 +1,21 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+// 长度至少为 k 的最大子数组和 (长度不超过 m 的版本见单调队列的同名文件)
+// i - j >= k => j <= i - k
+// 相当于在 [0, i-k] 范围找 s 的最小值
+// 不需要单调队列，因为左边界永远为 0, 只需要维护一个最小值即可
+int NoShorterThanK(const vector<int> &nums, int k) {
+    int n = static_cast<int>(nums.size());
+    vector<int> a(1), s(n + 1);
+    for (int x : nums) a.push_back(x);
+    for (int i = 1; i <= n; ++i) s[i] = s[i - 1] + a[i];
+
+    int ans = INT32_MIN;
+    int minPrefix = 0; // s[0]
+    for (int i = k; i <= n; ++i) {
+        minPrefix = min(minPrefix, s[i - k]);
+        ans = max(ans, s[i] - minPrefix);
+    }
+    return ans;
+}
