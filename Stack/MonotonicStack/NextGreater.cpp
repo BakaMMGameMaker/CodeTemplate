58c3dccd0a6acdc60e597ec57e099b67b1736e5f
Fuxuanmylove <2793275118@qq.com>
2026-03-11 20:45:35 +0800

    补充两道例题

--- Stack/MonotonicStack/NextGreater.cpp
+++ Stack/MonotonicStack/NextGreater.cpp
@@ -17,3 +17,37 @@ vector<int> nextGreaterElement(const vector<int> &nums) {
     }
     return ans;
 }
+
+// example 1 - 每一天往后等几天会变得更热
+vector<int> dailyTemperatures(const vector<int> &temperatures) {
+    const int   n = static_cast<int>(temperatures.size());
+    vector<int> ans(n, -1);
+    stack<int>  st;
+
+    for (int i = 0; i < n; ++i) {
+        while (!st.empty() && temperatures[st.top()] < temperatures[i]) {
+            ans[st.top()] = i - st.top();
+            st.pop();
+        }
+        st.push(i);
+    }
+    return ans;
+}
+
+// example 2 - 循环数组的 NextGreater
+// 把数组遍历两遍即可
+vector<int> nextGreaterElementII(const vector<int> &nums) {
+    const int   n = static_cast<int>(nums.size());
+    vector<int> ans(n, -1);
+    stack<int>  st;
+
+    for (int i = 0; i < 2 * n; ++i) {
+        const int x = nums[i % n];
+        while (!st.empty() && nums[st.top()] < x) {
+            ans[st.top()] = x; // 要下标就 i%n
+            st.pop();
+        }
+        if (i < n) st.push(i); // 每个元素只需要进一次栈
+    }
+    return ans;
+}
