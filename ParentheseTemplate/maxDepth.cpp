5bed7041796d1d03f1c884b39e23d6449b18c7fb
Fuxuanmylove <2793275118@qq.com>
2026-03-11 18:48:08 +0800

    括号的最大深度

---
+++ ParentheseTemplate/maxDepth.cpp
@@ -0,0 +1,16 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+// 计算括号的最大嵌套深度
+// 遇到左括号加一，遇到右括号减一
+
+int maxDepth(const string &s) {
+    int depth = 0, ans = 0;
+    for (const char c : s) {
+        if (c == '(') {
+            depth++;
+            ans = max(ans, depth);
+        } else if (c == ')') { depth--; }
+    }
+    return ans;
+}
