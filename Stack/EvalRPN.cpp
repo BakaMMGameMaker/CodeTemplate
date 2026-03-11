5240e94624d85c323e309d285caafefe5145668d
Fuxuanmylove <2793275118@qq.com>
2026-03-11 23:09:29 +0800

    逆波兰表达式求值

---
+++ Stack/EvalRPN.cpp
@@ -0,0 +1,22 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+// 逆波兰表达式求值
+
+int evalRPN(const vector<string> &tokens) {
+    stack<int> st;
+    for (const string &s : tokens) {
+        if (s == "+" || s == "-" || s == "*" || s == "/") { // operation
+            int b = st.top();
+            st.pop();
+            int a = st.top();
+            st.pop();
+            if (s == "+") st.push(a + b);
+            else if (s == "-") st.push(a - b);
+            else if (s == "*") st.push(a * b);
+            else st.push(a / b);
+        }
+        else { st.push(stoi(s)); }
+    }
+    return st.top();
+}
