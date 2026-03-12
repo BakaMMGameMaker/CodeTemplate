fa2d5c7cdc8039d44207e85f4e07f29045258e7c
Fuxuanmylove <2793275118@qq.com>
2026-03-12 23:33:43 +0800

    链表插入排序

---
+++ LinkedList/InsertionSort.cpp
@@ -0,0 +1,23 @@
+﻿#include <bits/stdc++.h>
+#include "ListNode.h"
+using namespace std;
+
+// 链表的插入排序
+
+ListNode *insertionSort(ListNode *head) {
+    // 思路 - prev 作为目标位置的前驱
+    // cur 为当前插入的节点
+    ListNode dm;
+    auto cur = head;
+    while (cur) {
+        auto nxt = cur->next;
+        // 在 dm 中找自己的位置
+        auto prev = &dm;
+        // 步进条件 - 下一个节点没自己大 严格小于防止无意义多跑
+        while (prev->next and prev->next->val < cur->val) { prev = prev->next; }
+        cur = prev->next;
+        prev->next = cur;
+        cur = nxt;
+    }
+    return dm.next;
+}
