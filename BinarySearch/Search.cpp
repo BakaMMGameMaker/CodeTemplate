dfc31ac36b399fcb48a3509adbb533984bf56a89
Fuxuanmylove <2793275118@qq.com>
2026-03-13 01:09:01 +0800

    完善了注释

--- BinarySearch/Search.cpp
+++ BinarySearch/Search.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // 二分查找基础
-// 二分查找最好用闭区间，不要给自己没事找事
+// 区间究竟是开区间还是闭区间，取决于答案可能落在哪里
 // while 条件为 < 还是 <= 主要取决于 r 的更新方式
 // 如果有充足证据证明 mid 位置不可能为结果，那么 r = mid - 1，否则 r = mid
 int search(const vector<int> &nums, int target) {
