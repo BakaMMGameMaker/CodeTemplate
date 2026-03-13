8eeb0bcc5387c8deb1e3d53c48653260ba94de25
Fuxuanmylove <2793275118@qq.com>
2026-03-14 00:16:27 +0800

    对称二叉树

--- BinaryTree/Judge.cpp
+++ BinaryTree/Judge.cpp
@@ -33,3 +33,47 @@ bool isSameTreeBFS(const TreeNode* p, const TreeNode* q) {
     }
     return true;
 }
+
+// 对称二叉树 - 两棵树是否镜像
+// 思路 - 左子树的左边是否等于右子树的右边
+// 左子树的右边是否等于右子树的左边
+bool isSymmetric(const TreeNode* root) {
+    if (not root) return true;
+
+    function<bool(const TreeNode*, const TreeNode*)> isMirror = [&](
+        const TreeNode* p, const TreeNode* q) {
+        if (not p and not q) return true;
+        if (not p or not q) return false;
+        if (p->val != q->val) return false;
+
+        return isMirror(p->left, q->right) and isMirror(p->right, q->left);
+    };
+
+    return isMirror(root->left, root->right);
+}
+
+// BFS 解法
+bool isSymmetricBFS(const TreeNode* root) {
+    if (not root) return true;
+
+    deque q{root->left, root->right};
+
+    while (!q.empty()) {
+        auto l = q.front();
+        q.pop_front();
+        auto r = q.front();
+        q.pop_front();
+
+        if (not l and not r) continue;
+        if (not l or not r) return false;
+        if (l->val != r->val) return false;
+
+        // 注意添加顺序
+        q.push_back(l->left);
+        q.push_back(r->right);
+
+        q.push_back(l->right);
+        q.push_back(r->left);
+    }
+    return true;
+}
