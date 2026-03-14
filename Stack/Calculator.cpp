965e25111ff528c28f69f9109f09d516a27104cc
Fuxuanmylove <2793275118@qq.com>
2026-03-15 02:18:57 +0800

    中缀转后缀

--- Stack/Calculator.cpp
+++ Stack/Calculator.cpp
@@ -11,14 +11,12 @@ using namespace std;
 // 遇到右括号, 不断计算直到弹出左括号
 // 遇到普通运算符, 根据优先级把栈内优先级更高或者相等的运算做掉, 再把当前运算符入栈
 // 最后做完剩余的运算
-
 int calculate(const string &s) {
     stack<int> nums;
     stack<char> ops;
     size_t n = s.size();
-    unordered_map<char, int> priority{
-        {'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}
-    };
+
+    unordered_map<char, int> priority{{'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}};
 
     auto eval = [&] {
         int b = nums.top();
@@ -70,6 +68,49 @@ int calculate(const string &s) {
 
     while (!ops.empty()) eval();
     return nums.top();
+}
+
+// 中缀转后缀 (RPN)
+vector<string> infixToPostfix(const string &s) {
+    vector<string> output;
+    stack<char> ops;
+    size_t n = s.size();
+
+    unordered_map<char, int> priority{{'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}};
+
+    for (size_t i = 0; i < n; ++i) {
+        if (s[i] == ' ') continue;
+
+        if (isdigit(s[i])) {
+            string num;
+            while (i < n and isdigit(s[i])) {
+                num += s[i];
+                i++;
+            }
+            i--;
+            output.push_back(num);
+        } else if (s[i] == '(') {
+            ops.push(s[i]);
+        } else if (s[i] == ')') {
+            while (not ops.empty() and ops.top() != '(') {
+                output.emplace_back(1, ops.top());
+                ops.pop();
+            }
+            if (not ops.empty()) ops.pop(); // 弹出左括号
+        } else {
+            while (!ops.empty() and ops.top() != '(' and priority[ops.top()] >= priority[s[i]]) {
+                output.emplace_back(1, ops.top());
+                ops.pop();
+            }
+            ops.push(s[i]);
+        }
+    }
+
+    while (not ops.empty()) {
+        output.emplace_back(1, ops.top());
+        ops.pop();
+    }
+    return output;
 }
 
 // 逆波兰表达式求值
