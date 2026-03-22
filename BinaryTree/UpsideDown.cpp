d98d4d0e828fdcc56ee05e2fa74f3934b5604b72
Fuxuanmylove <2793275118@qq.com>
2026-03-22 19:15:28 +0800

    上下翻转二叉树

---
+++ BinaryTree/UpsideDown.cpp
@@ -0,0 +1,34 @@
+﻿#include "TreeNode.h"
+
+// 上下翻转二叉树
+
+TreeNode *upsidedown(TreeNode *root) {
+    if (not root or not root->left) return root;
+
+    TreeNode *newroot = upsidedown(root->left);
+    root->left->left = root->right;
+    root->left->right = root;
+
+    root->left = nullptr;
+    root->right = nullptr;
+
+    return newroot;
+}
+
+TreeNode *upsidedowniterative(TreeNode *root) {
+    TreeNode *curr = root;
+    TreeNode *parent = nullptr;
+    TreeNode *parentright = nullptr;
+
+    while (curr) {
+        TreeNode *next = curr->left;
+        curr->left = parentright;
+        parentright = curr->right;
+
+        curr->right = parent;
+        parent = curr;
+
+        curr = next;
+    }
+    return parent;
+}
